c98e6fe36eb266bf37a5410bb89867760a4a869c
Edson <edsaraujocorral12@gmail.com>
2020-06-11 15:51:27 -0300

    Utilizando LocalStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -6,12 +6,30 @@ import api from '../../services/api';
 import { Conteiner, Form, SubmitButton, List } from './styles';
 
 class Main extends Component {
-
   state = {
     newRepo: '',
     repositories: [],
     loading: false,
   };
+
+  // Carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+      console.log('test');
+    }
+  }
+
+  // Salvar os dados do localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
