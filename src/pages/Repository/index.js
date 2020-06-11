4890b7fea5b6bc435a4667ec70653146a87486f7
Edson <edsaraujocorral12@gmail.com>
2020-06-11 16:00:46 -0300

    Navegacao de Rotas

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Repository() {
-  return <h1>Repository Page</h1>;
+function Repository({ match }) {
+  return <h1>Repository Page: {decodeURIComponent(match.params.repository)}</h1>;
 }
 export default Repository;
